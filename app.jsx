917728f1e0d24eb81866f6a7ef10b0dfbee127bb
Abigail <abi25.fly@gmail.com>
2017-09-29 10:49:48 -0500

    second

--- app.jsx
+++ app.jsx
@@ -1,30 +1,78 @@
-let PLAYERS = [
+let players = [
   {
     name: "Jim Hoskins",
     score: 31,
     id: 1,
   },
-   {
+  {
     name: "Andree Hoskins",
     score: 35,
     id: 2,
   },
-   {
+  {
     name: "Alena Hoskins",
     score: 42,
     id: 3,
   },
 ];
 
-
-
-const Application = ({title, players}) => {
-   return (
-      <Header players={players}/>
-      <h1>Hola</h1>
-      <PlayerList players={players}/>
-      <PlayerForm />      
-   ) ;
+const Header = ({players}) => {
+  return (
+    <div className="header">
+      <table className="stats">
+        <tr>
+          <td>PLAYERS:</td>
+          <td>3</td>
+        </tr>
+        <tr>
+          <td>TOTAL POINTS:</td>
+          <td>108</td>
+        </tr>
+      </table>
+      <div className="stopwatch">
+        <h2>STOPWATH</h2>
+        <h1 className="stopwatch-time">0</h1>
+        <button>START</button>
+        <button>RESET</button>
+      </div>
+    </div>
+  );
 }
 
-ReactDOM.render(<Application title="Scoreboard" players = {PLAYERS}/>, document.getElementById('container'));
+
+const PlayerList = ({players}) => {
+  return (
+    <div className="player">
+      <div className="player-name">Jim Hoskins</div>
+      <div className="player-score counter">
+        <button className="counter-action decrement">-</button>
+        <span className="counter-score">31</span>
+        <button className="counter-action increment">+</button>
+      </div>
+    </div>
+  );
+}
+
+
+const PlayerForm= () =>{
+  return (
+    <div className='add-player-form'>
+    <form>
+      <input type="text" placeholder='ENTER A NAME'/>
+      <input type="submit" value='add player' />
+    </form>
+  </div>
+  );
+}
+
+const Application = ({ title, players }) => {
+  return (
+    <div className="scoreboard">
+      <Header players={players} />
+      <PlayerList players={players}/>
+      <PlayerForm /> 
+    </div>
+  );
+}
+
+ReactDOM.render(<Application title="Scoreboard" players={players} />, document.getElementById('container'));
